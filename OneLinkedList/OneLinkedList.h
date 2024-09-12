7f5513d45f89d37799779c88f033a2cfb297ecda
petrov.v <phisicistupid@gmail.com>
2024-09-12 11:43:51 +0300

    bugix of OneLinkedList

--- OneLinkedList/OneLinkedList.h
+++ OneLinkedList/OneLinkedList.h
@@ -4,50 +4,60 @@
 #include "../ListObject/ListObject.h"
 #include <memory>
 
-template <typename T> 
+template <typename T>
 class OneLinkedList {
 public:
     OneLinkedList() : head_(nullptr), tail_(nullptr), size_(0) {}
 
-    OneLinkedList(const OneLinkedList &other)
-        : head_(other.head_), tail_(other.tail_) {
+    OneLinkedList(const OneLinkedList& other) : head_(nullptr), tail_(nullptr), size_(0) {
         auto currObj = other.head_;
-        while (currObj) {
-        push_back(currObj->get_data());
-        currObj = currObj->get_next();
-        }
-        size_ = 1;
-    }
+        std::shared_ptr<ListObject<T>> prevObj = nullptr;
 
-    OneLinkedList(OneLinkedList<T> &&other) noexcept
-        : head_(std::move(other.head_)), tail_(std::move(other.tail_)) {
-        other.head_ = nullptr;
-        other.tail_ = nullptr;
-        size_ = 1;
-    }
-
-    OneLinkedList<T> &operator=(const OneLinkedList<T> &other) {
-        if (this != &other) {
-        clear();
-        auto currObj = other.head_;
         while (currObj) {
-            push_back(currObj->get_data());
+            auto newObj = std::make_shared<ListObject<T>>(currObj->get_data());
+
+            if (!head_) {
+                head_ = newObj;
+            } else {
+                prevObj->set_next(newObj);
+            }
+            prevObj = newObj;
             currObj = currObj->get_next();
         }
-        }
+        tail_ = prevObj;
         size_ = other.size_;
+    }
+
+    OneLinkedList(OneLinkedList&& other) noexcept
+        : head_(std::move(other.head_)), tail_(std::move(other.tail_)), size_(other.size_) {
+        other.head_ = nullptr;
+        other.tail_ = nullptr;
+        other.size_ = 0;
+    }
+
+    OneLinkedList& operator=(const OneLinkedList& other) {
+        if (this != &other) {
+            clear();
+            auto currObj = other.head_;
+            while (currObj) {
+                push_back(currObj->get_data());
+                currObj = currObj->get_next();
+            }
+            size_ = other.size_;
+        }
         return *this;
     }
 
-    OneLinkedList<T> &operator=(OneLinkedList<T> &&other) noexcept {
+    OneLinkedList& operator=(OneLinkedList&& other) noexcept {
         if (this != &other) {
-        clear();
-        head_ = std::move(other.head_);
-        tail_ = std::move(other.tail_);
-        other.head_ = nullptr;
-        other.tail_ = nullptr;
+            clear();
+            head_ = std::move(other.head_);
+            tail_ = std::move(other.tail_);
+            size_ = other.size_;
+            other.head_ = nullptr;
+            other.tail_ = nullptr;
+            other.size_ = 0;
         }
-        size_ = other.size_;
         return *this;
     }
 
@@ -55,65 +65,66 @@ public:
 
     std::shared_ptr<ListObject<T>> get_head() const { return head_; }
 
-    int size() const { return size_; }
+    size_t size() const { return size_; }
 
-    void push_back(T const &data);
-    void push_back(const ListObject<T> &data);
+    void push_back(const T& data);
+    void push_back(const ListObject<T>& data);
 
-    void push_front(const ListObject<T> &obj);
-    void push_front(T const &data);
+    void push_front(const ListObject<T>& obj);
+    void push_front(const T& data);
 
     void pop_back();
     void pop_front();
 
-    private:
+private:
     std::shared_ptr<ListObject<T>> head_;
     std::shared_ptr<ListObject<T>> tail_;
-    int size_;
+    size_t size_;
 
     void clear() {
         while (head_)
             pop_front();
     }
 
-    class Item{
-        int index_{0};
+    class Item {
+        size_t index_{0};
         std::shared_ptr<OneLinkedList<T>> current_{nullptr};
-        
-        public:
 
-        Item(OneLinkedList<T> *ptr, int index = 0) : current_(ptr), index_(index) {}
+    public:
+        Item(OneLinkedList<T>* ptr, size_t index = 0) : current_(ptr), index_(index) {}
 
         operator T() const {
-            if(index_ >= current_->size() || index_ < 0)
+            if (index_ >= current_->size() || index_ < 0)
                 throw LinkedListIndexError("Invalid element index.");
             auto currObj = current_->get_head();
-            for (int i = 0; i < index_ && currObj; ++i) {
+            for (size_t i = 0; i < index_ && currObj; ++i) {
                 currObj = currObj->get_next();
             }
             return currObj->get_data();
         }
 
         T operator=(const T value) {
-            if(index_ >= current_->size() || index_ < 0)
+            if (index_ >= current_->size() || index_ < 0)
                 throw LinkedListIndexError("Invalid element index.");
             auto currObj = current_->get_head();
-            for (int i = 0; i < index_ && currObj; ++i) {
+            for (size_t i = 0; i < index_ && currObj; ++i) {
                 currObj = currObj->get_next();
             }
             currObj->set_data(value);
             return value;
         }
     };
-    public:
 
-    Item operator[](const int i) {
+public:
+    Item operator[](size_t i) {
         return Item(this, i);
     }
+
+private:
 };
 
-// Implementation
-template <typename T> void OneLinkedList<T>::push_back(T const &data) {
+template <typename T>
+void OneLinkedList<T>::push_back(const T& data) {
     auto newObj = std::make_shared<ListObject<T>>(data);
     if (tail_) {
         tail_->set_next(newObj);
@@ -125,7 +136,7 @@ template <typename T> void OneLinkedList<T>::push_back(T const &data) {
 }
 
 template <typename T>
-void OneLinkedList<T>::push_back(const ListObject<T> &data) {
+void OneLinkedList<T>::push_back(const ListObject<T>& data) {
     auto newObj = std::make_shared<ListObject<T>>(data);
     if (tail_) {
         tail_->set_next(newObj);
@@ -137,7 +148,7 @@ void OneLinkedList<T>::push_back(const ListObject<T> &data) {
 }
 
 template <typename T>
-void OneLinkedList<T>::push_front(const ListObject<T> &data) {
+void OneLinkedList<T>::push_front(const ListObject<T>& data) {
     auto newObj = std::make_shared<ListObject<T>>(data, head_);
     if (!tail_) {
         tail_ = newObj;
@@ -146,7 +157,8 @@ void OneLinkedList<T>::push_front(const ListObject<T> &data) {
     size_++;
 }
 
-template <typename T> void OneLinkedList<T>::push_front(T const &data) {
+template <typename T>
+void OneLinkedList<T>::push_front(const T& data) {
     auto newObj = std::make_shared<ListObject<T>>(data, head_);
     if (!tail_) {
         tail_ = newObj;
@@ -155,7 +167,8 @@ template <typename T> void OneLinkedList<T>::push_front(T const &data) {
     size_++;
 }
 
-template <typename T> void OneLinkedList<T>::pop_back() {
+template <typename T>
+void OneLinkedList<T>::pop_back() {
     if (!head_)
         return;
 
@@ -175,7 +188,7 @@ template <typename T> void OneLinkedList<T>::pop_back() {
     size_--;
 }
 
-template <typename T> 
+template <typename T>
 void OneLinkedList<T>::pop_front() {
     if (!head_)
         return;
